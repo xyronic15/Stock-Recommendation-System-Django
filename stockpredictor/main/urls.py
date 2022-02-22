010def3d5a2e8c214f88197e76eb5fffb6cab5cf
xyronic15 <xyronb@gmail.com>
2022-02-22 16:45:52 -0500

    chagned url for home page

--- stockpredictor/main/urls.py
+++ stockpredictor/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("home", views.home, name="home"),
     path("stock/<ticker>", views.stock, name="stock"),
     path("predict/", views.predict, name="predict"),
 ]
