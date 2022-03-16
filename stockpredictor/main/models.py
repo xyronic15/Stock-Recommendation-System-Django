6b966dfb22341961122b6f043f9f86493b1279bc
xyronic15 <xyronb@gmail.com>
2022-03-16 10:16:07 -0400

    migrated new favourite model

--- stockpredictor/main/models.py
+++ stockpredictor/main/models.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Favourite(models.Model):
     userID = models.ForeignKey(User, on_delete=models.CASCADE, related_name="favourite")
-    ticker  = models.CharField(max_length=200)
+    ticker  = models.CharField(max_length=200, default="")
+    longName = models.CharField(max_length=200, default="")
+    logo_url = models.CharField(max_length=200, default="")
 
     def __str__(self):
         return self.ticker
